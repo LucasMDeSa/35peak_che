b30f47afde608c88533c048da9ba236843ae81d8
LucasMDeSa <lucas.marquesanto@gmail.com>
2026-09-22 10:28:08 +0200

    chore(scripts): add map grids 01 and 02 getter batch scripts

---
+++ scripts/get_01_map_core_props.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p cpu-single
+#SBATCH -t 00:30:00
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 16
+#SBATCH --mem 30G
+
+source ~/.bashrc
+pyenv activate cher
+
+settings_path="get_core_properties_v7_settings.yaml"
+srun python get_core_properties_v7.py --map-grid --output-title core_props_df_01_map_v7.h5 --n-cores $SLURM_CPUS_PER_TASK --model 01_weak_mixing --model-prefix 01 --use-depletion-profiles TRUE --settings $settings_path
